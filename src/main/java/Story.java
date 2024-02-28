00f164f4bd1d93ceb169c343b4cd5a12c113f6ed
Kipot22 <k4872319@gmail.com>
2024-02-28 21:09:38 +0100

    new2 commit

--- src/main/java/Story.java
+++ src/main/java/Story.java
@@ -5,7 +5,12 @@ public class Story extends Task{
     private int storyId;
 
 
-    public Story(int taskId, String taskType, String taskName, String taskComments, String taskAssignedTo, description, List<SubTask> subTasks, int storyId) {
-        super(taskId, taskType, taskName, taskComments, taskAssignedTo, description);
+    public Story(int taskId, String taskType, String taskName, String taskComments, String taskAssignedTo, String description, List<SubTask> subTasks, int storyId) {
+        super(taskId,
+                taskType,
+                taskName,
+                taskComments,
+                taskAssignedTo,
+                getDescription());
     }
 }
