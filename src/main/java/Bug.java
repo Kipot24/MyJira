c25e1abbd36021826504f52a214f372313e897b7
Kipot24 <162017054+Kipot24@users.noreply.github.com>
2024-03-04 21:05:23 +0100

    Revert "Add test1 commit"
    This reverts commit c50f616afdce310da49129f8cd0afda90ec7c9d6.

--- src/main/java/Bug.java
+++
@@ -1,9 +0,0 @@
-public class Bug extends Task{
-    String priority;
-    int severity;
-
-
-    public Bug(int taskId, String taskType, String taskName, String taskComments, String taskAssignedTo, Type type) {
-        super(taskId, taskType, taskName, taskComments, taskAssignedTo, type);
-    }
-}
--- src/main/java/Story.java
+++
@@ -1,7 +0,0 @@
-public class Story extends Task{
-    String description;
-
-    public Story(int taskId, String taskType, String taskName, String taskComments, String taskAssignedTo, Type type) {
-        super(taskId, taskType, taskName, taskComments, taskAssignedTo, type);
-    }
-}
